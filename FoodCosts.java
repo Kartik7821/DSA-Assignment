d0f545f0a1101f36d68fc0b0e3675103fb1311b7
Kartik Saha <75121886+Kartik7821@users.noreply.github.com>
2024-04-15 14:57:22 +0530

    Create FoodCosts.java

---
+++ FoodCosts.java
@@ -0,0 +1,13 @@
+// Food Costs
+
+import java.util.*;
+
+public class FoodCosts {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x * 6 + y);
+        sc.close();
+    }
+}
